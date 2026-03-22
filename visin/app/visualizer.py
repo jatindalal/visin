22ac31b7361659e9af4c8441d287a4692c6db1a1
Jatin Dalal <jd.jatindalal@gmail.com>
2026-03-22 12:00:53 +0530

    fix resize bug in imgui render

--- visin/app/visualizer.py
+++ visin/app/visualizer.py
@@ -13,6 +13,8 @@ class Visualizer:
 
         self.window = None
         self.ctx = None
+        self.imgui_io = None
+        self.imgui_glfw = None
 
     def run(self):
         self._init_window()
@@ -22,16 +24,31 @@ class Visualizer:
             glfw.poll_events()
             self.imgui_glfw.process_inputs()
 
-            self.ctx.clear(0.3, 0.3, 0.3)
-
-            imgui.new_frame()
-            self._render_ui()
-            imgui.render()
-
-            self.imgui_glfw.render(imgui.get_draw_data())
-            glfw.swap_buffers(self.window)
+            fb_width, fb_height = glfw.get_framebuffer_size(self.window)
+            self._render(fb_width, fb_height)
 
         self._shutdown()
+
+    def _render(self, fb_width, fb_height):
+        win_width, win_height = glfw.get_window_size(self.window)
+        if win_width == 0 or win_height == 0:
+            return
+
+        self.ctx.viewport = (0, 0, fb_width, fb_height)
+        self.imgui_io.display_size = (win_width, win_height)
+        self.imgui_io.display_framebuffer_scale = (
+            fb_width / win_width,
+            fb_height / win_height,
+        )
+
+        self.ctx.clear(0.3, 0.3, 0.3)
+
+        imgui.new_frame()
+        self._render_ui()
+        imgui.render()
+        self.imgui_glfw.render(imgui.get_draw_data())
+
+        glfw.swap_buffers(self.window)
 
     def _render_ui(self):
         imgui.show_demo_window()
@@ -39,8 +56,8 @@ class Visualizer:
     def _init_imgui(self):
         imgui.create_context()
 
-        io = imgui.get_io()
-        io.config_flags |= imgui.ConfigFlags_.docking_enable
+        self.imgui_io = imgui.get_io()
+        self.imgui_io.config_flags |= imgui.ConfigFlags_.docking_enable
         self.imgui_glfw = GlfwRenderer(self.window, attach_callbacks=True)
 
     def _init_window(self):
@@ -52,6 +69,7 @@ class Visualizer:
         glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
         glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
         glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+        # glfw.window_hint(glfw.COCOA_RETINA_FRAMEBUFFER, glfw.TRUE)
 
         # create a window
         self.window = glfw.create_window(
@@ -62,10 +80,13 @@ class Visualizer:
             raise RuntimeWarning("Could not create Window")
 
         glfw.make_context_current(self.window)
-
         glfw.swap_interval(1)
+        glfw.set_framebuffer_size_callback(self.window, self._on_resize)
 
         self.ctx = moderngl.create_context()
+
+    def _on_resize(self, window, fb_width, fb_height):
+        self._render(fb_width, fb_height)
 
     def _shutdown(self):
         glfw.terminate()
